9238827738f7dc44b5a1f7f9eb48b38a84993a02
Rafael Monteiro <rmonteiro811@gmail.com>
2025-06-16 17:43:53 +0100

    put some fake keys on files

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': sk-FAKE_TEST_KEY_1234567890abcdef,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
